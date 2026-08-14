eac4cf64f2face609c87731f4f812323cb754fcb
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2026-08-14 19:36:42 +0200

    refactor menu items: swap "Cull Similar Photos" with "Find Similar Images" and restore "Import Metadata" and "Export Culling Fixture"

--- plugin/LrGeniusAI.lrdevplugin/Info.lua
+++ plugin/LrGeniusAI.lrdevplugin/Info.lua
@@ -35,20 +35,16 @@ return {
 			file = "TaskSemanticSearch.lua",
 		},
 		{
+			title = LOC("$$$/LrGeniusAI/Menu/FindSimilarImages=Find Similar Images..."),
+			file = "TaskFindSimilarImages.lua",
+		},
+		{
 			title = LOC("$$$/LrGeniusAI/Menu/CullPhotos=Cull Similar Photos..."),
 			file = "TaskCullPhotos.lua",
 		},
 		{
-			title = LOC("$$$/LrGeniusAI/Menu/ExportCullFixture=Export Culling Fixture..."),
-			file = "TaskExportCullFixture.lua",
-		},
-		{
 			title = LOC("$$$/LrGeniusAI/Menu/RetrieveMetadata=Retrieve Metadata from Backend..."),
 			file = "TaskRetrieveMetadata.lua",
-		},
-		{
-			title = LOC("$$$/LrGeniusAI/Menu/ImportMetadata=Import Metadata from Catalog..."),
-			file = "TaskImportMetadata.lua",
 		},
 		{
 			title = LOC("$$$/LrGeniusAI/Menu/People=People..."),
@@ -57,10 +53,6 @@ return {
 		{
 			title = LOC("$$$/LrGeniusAI/Menu/FindSimilarFaces=Find Similar Faces..."),
 			file = "TaskFindSimilarFaces.lua",
-		},
-		{
-			title = LOC("$$$/LrGeniusAI/Menu/FindSimilarImages=Find Similar Images..."),
-			file = "TaskFindSimilarImages.lua",
 		},
 		{
 			title = LOC("$$$/LrGeniusAI/Training/MenuItem=Save Edits as AI Training Examples..."),
@@ -127,6 +119,14 @@ return {
 		{
 			title = "Developer: Run Automated Tests...",
 			file = "TaskAutomatedTests.lua",
+		},
+		{
+			title = LOC("$$$/LrGeniusAI/Menu/ImportMetadata=Import Metadata from Catalog..."),
+			file = "TaskImportMetadata.lua",
+		},
+		{
+			title = LOC("$$$/LrGeniusAI/Menu/ExportCullFixture=Export Culling Fixture..."),
+			file = "TaskExportCullFixture.lua",
 		},
 	},
 
